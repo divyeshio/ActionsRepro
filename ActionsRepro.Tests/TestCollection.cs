8ff75fef10f865e028b357a792281cc2ed2a39ae
divyeshio <79130336+divyeshio@users.noreply.github.com>
2025-11-21 03:06:32 +0530

    Remove parallel test framework reference and disable parallelization in test collection

--- ActionsRepro.Tests/TestCollection.cs
+++ ActionsRepro.Tests/TestCollection.cs
@@ -1,12 +1,6 @@
-﻿using Meziantou.Xunit.v3;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ActionsRepro.Tests;
+﻿namespace ActionsRepro.Tests;
 
 [CollectionDefinition(nameof(TestCollection))]
-[EnableParallelization]
 public class TestCollection : ICollectionFixture<TestFixture>
 {
 }
