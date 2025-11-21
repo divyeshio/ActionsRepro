46d5e6f2daf949a0ca884307adacb811a1584b83
divyeshio <79130336+divyeshio@users.noreply.github.com>
2025-11-21 18:41:01 +0530

    Add PostgreSQL package reference and configure database connection in AppHost

--- ActionsRepro.AppHost/AppHost.cs
+++ ActionsRepro.AppHost/AppHost.cs
@@ -1,6 +1,27 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var password = builder.AddParameter("db-password", true);
+
+#pragma warning disable ASPIREPROXYENDPOINTS001
+var dbServer = builder.AddPostgres("postgres-server", port: 5432, password: password)
+    .WithImageTag("18.0")
+    .WithEndpointProxySupport(false)
+#pragma warning restore ASPIREPROXYENDPOINTS001
+    .WithLifetime(ContainerLifetime.Persistent);
+
+dbServer.WithVolume(VolumeNameGenerator.Generate(dbServer, "data"),
+            "/var/lib/postgresql/18/docker", false);
+
+var database = dbServer.AddDatabase("db");
+
+var pgConnectionString = builder.AddConnectionString(
+    "database",
+    ReferenceExpression.Create($"{database};Include Error Detail=true"));
+
+
 var apiService = builder.AddProject<Projects.ActionsRepro_ApiService>("apiservice")
+    .WithReference(pgConnectionString)
+    .WaitFor(pgConnectionString)
     .WithHttpHealthCheck("/health");
 
 builder.AddProject<Projects.ActionsRepro_Web>("webfrontend")
